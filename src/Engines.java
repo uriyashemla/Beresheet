59ae07c35d38a9ace2a32fba21838f6b104b182f
Nadav-Eps <nadavepstein1@gmail.com>
2022-05-14 13:21:50 +0300

    engine code

--- src/Engines.java
+++ src/Engines.java
@@ -1,3 +1,17 @@
 public class Engines {
+    private String side;
+    private double power;
 
+    NavigationEngine(String side, double power) {
+        this.side = side;
+        this.power = power;
+    }
+
+    public double getPower() {
+        return this.power;
+    }
+
+    public void setPower(double power) {
+        this.power = power;
+    }
 }
